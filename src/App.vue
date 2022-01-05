0586b0ce00114fa877449652f429b8722a70cbb5
Togni <mathis.togni@sfr.fr>
2022-01-05 10:31:38 +0100

    ajout http | locomotive

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import locomotiveScroll from "locomotive-scroll";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    console.log("test");
+
+    this.$nextTick(() => {
+      console.log("test2");
+
+      this.initLocomotiveScroll();
+      console.log("test3");
+    });
+  },
+  methods: {
+    initLocomotiveScroll() {
+      window.lmS = new locomotiveScroll({
+        el: document.querySelector("#app"),
+        smooth: true,
+        tablet: {
+          smooth: false,
+        },
+        smartphone: { smooth: false },
+        lerp: 0.002,
+      });
+    },
+  },
+};
 </script>
 
 <style>
