7a56ad65a56983048f1b6f464a8c9ae9ff3b4d48
Coralie-V <coralie.varrier@edu.univ-fcomte.fr>
2022-01-19 11:42:55 +0100

    page mes modèles

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,15 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/Inspirations.vue"),
   },
   {
+    path: "/modeles",
+    name: "Modeles",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Modeles.vue"),
+  },
+  {
     path: "/connexion",
     name: "Connexion",
     // route level code-splitting
---
+++ src/views/Modeles.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="container min-height">
+    <div class="inspirations">
+      <div id="titre_page">
+        <div class="trait_titre_h1"></div>
+        <h1 id="titre_h1">Mes modèles</h1>
+        <div class="trait_titre_h1"></div>
+      </div>
+
+        <div class="col-md-8 inspirations__block --modele">
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+          <div class="inspirations__item">
+            <img
+              class="inspirations__img"
+              src="@/assets/images/header-basket.png"
+              alt="image chaussure"
+            />
+            <div>
+              <p>13/12/2021</p>
+              <p>Par Anonyme</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-2">
+          <a href="#">Voir plus</a>
+        </div>
+    </div>
+  </section>
+</template>
